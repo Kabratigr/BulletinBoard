f94e1cfcb5b9aefb6c34babd053101348be0e511
Kabratigr\Vadidya <kabratigr746@gmail.com>
2024-11-05 22:12:47 +0200

    Fixed Preview Image Display

--- src/main/java/com/vadym/board/controllers/ImageRestController.java
+++ src/main/java/com/vadym/board/controllers/ImageRestController.java
@@ -1,22 +1,28 @@
 package com.vadym.board.controllers;
 
+import com.vadym.board.models.Announcement;
 import com.vadym.board.models.Image;
+import com.vadym.board.repositories.AnnouncementRepository;
 import com.vadym.board.repositories.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageRestController {
 
     private final ImageRepository imageRepository;
+
+    private final AnnouncementRepository announcementRepository;
 
     @GetMapping("/images/{id}")
     private ResponseEntity<?> getImageById(@PathVariable Long id) {
@@ -26,5 +32,22 @@ public class ImageRestController {
                 .contentType(MediaType.valueOf(image.getContentType()))
                 .contentLength(image.getImageSize())
                 .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
+    }
+
+    @DeleteMapping("/images/{id}")
+    public ResponseEntity<?> deleteImage(@PathVariable Long id) {
+        Optional<Image> imageOptional = imageRepository.findById(id);
+        if (imageOptional.isPresent()) {
+            Image image = imageOptional.get();
+            Announcement announcement = image.getAnnouncement();
+            announcement.getImages().remove(image);
+            imageRepository.delete(image);
+            if (image.isPreviewImage()) {
+                announcement.setPreviewImageId(null);
+            }
+            announcementRepository.save(announcement);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
